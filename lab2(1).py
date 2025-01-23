3e60764400cce2771810d86c6cb34601129db99d
Rachana240906 <u24ai064@aid.svnit.ac.in>
2025-01-23 15:00:48 +0530

    Create lab2(1).py

---
+++ lab2(1).py
@@ -0,0 +1,15 @@
+#1(a)
+list_a = []
+for i in range(50):
+    list_a.append(i)
+print(list_a)
+#1(b)
+list_b = []
+for i in range(1, 51):
+    list_b.append(i ** 2)
+print(list_b)
+#1(c)
+list_c = []
+for i in range(1, 27):
+    list_c.append(chr(96 + i) * i)
+print(list_c)
