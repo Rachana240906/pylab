530115aaec887e225c61cc014d756bb92236131d
Rachana240906 <u24ai064@aid.svnit.ac.in>
2025-04-02 17:34:54 +0530

    Create ass 9(3).py

---
+++ ass 9(3).py
@@ -0,0 +1,47 @@
+HTML:
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amazon</title>
+    <link rel="stylesheet" href="amazon_style.css">
+</head>
+<body>
+    <pre>
+    <img src="https://cdn.iconscout.com/icon/free/png-256/free-amazon-logo-icon-download-in-svg-png-gif-file-formats--major-websites-set-pack-logos-icons-461766.png"height="100"width="50">  <img src="https://cdn3.vectorstock.com/i/1000x1000/48/77/simple-search-bar-with-magnifying-glass-vector-45254877.jpg"width="940"height="100"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB4_CBl4POx3zbzfPn3gPuWQJJsRcgwqDTbQ&s"width="100"height="100"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpuSIXINMWa-reBrgX9wlyL6mcYoVrhqUPIg&s"width="100"height="100">
+    <a href="https://www.amazon.in/gp/help/customer/display.html?nodeId=GSUNWNFT2ALMPR3L"><H3>SEARCH</H3></a>
+    </pre>
+    <hr>
+    <img src="https://www.livehindustan.com/lh-img/smart/img/2024/05/06/1200x900/Free-OTT-Subscription-Amazon_1715008202640_1715008217266.jpeg"width="600"height="300">  <img src="https://images.moneycontrol.com/static-mcnews/2024/07/20240725074952_WhatsApp-Image-2024-07-25-at-12.17.26.jpeg"width="600"height="300">
+    <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwi3m_yEjaqMAxUNDoMDHYZAPHkYABAAGgJzZg&co=1&gclid=CjwKCAjw7pO_BhAlEiwA4pMQvLgXE8LCGM0DnFzX6_G4T3o2wSAC_2Tr4ehijyoVBdb54rbBLa9vDBoCAT0QAvD_BwE&ohost=www.google.com&cid=CAESVuD2TqL3tzFFvA310EV3ksgqlkmQlJFf3gjof0KHFUHLVWNeaU--LxKhhMUN5dF34n5oAersaNuPBR21nhqY33qskBN1v1Z6er3gnK-rG4ZuHlGO_abB&sig=AOD64_0Rmr1qO2fs517kvI22oPBm69ODaA&q&adurl&ved=2ahUKEwjAidCEjaqMAxUqcGwGHSbLIGYQ0Qx6BAggEAE"><h3>AMAZON PRIME VIDEO</h3></a>
+    <hr>
+    <pre>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8qcK5S6qFAYjn9b4Da0z8jBcXNRKHMirKIg&s"width="300"height="300">          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbj2-uk84_McKkbo7GWF6Zr6R1MwgMcGDT4Q&s"width="300"height="300">          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYqlBgsFEzBtyoLws3GEHF-jBj5478eS1CTA&s"width="300"height="300">
+    <a href="https://www.amazon.in/Clothing-accesories/b?ie=UTF8&node=1571271031"><h3>CLOTHING</h3></a>
+    <hr>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIBP1NciMfVGSOvgPBWHPZRDNjYr0PsKsuBg&s"width="300"height="300">          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy-mxBMm-GdDfGCA5As0_tm0VzUV_DM7tl-g&s"width="300"height="300">          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe4jYPjjCPY9gg6dbWyJwM1cox3kfmAlIj1g&s"width="300"height="300">
+    <a href="https://www.amazon.in/Kitchen-Dining-Store/b?ie=UTF8&node=5925789031"><h3>KITCHEN ESSENTIALS</h3></a>
+    <hr>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyZE_ghg-GHbUaDCk-JQ05CZgcpwTcRPNUJg&s"width="300"height="300">          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi06_zomh4dpNeF859I6LB60ELoE4Sx0cezA&s"width="300"height="300">          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN2tyL8pI8gFcyVMUaxmn4NuJ4GSybNAgLsw&s"width="300"height="300">
+    <a href="https://www.amazon.in/Home-D%C3%A9cor/b?ie=UTF8&node=1380374031"><h3>HOME DECOR</h3></a>
+    </pre>
+    <hr>
+    <p style="display: inline-block;"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsB_pL-fvWE3x3qR1luT0ZRW9OkupMjaxk0g&s"width="20"height="20"><h2>:amazon@queries.com</h2></p>
+    <hr>
+    <p style="display: inline-block;"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7QCNc2PcfWtT5hcCi5exJlN7NL65oegOHQA&s"width="20"height="20"><h2>:+91 9030257090</h2></p>
+</body>
+</html>
+
+
+CSS:
+body{
+    background:linear-gradient(to left,rgb(119, 215, 247),white,rgb(119, 215, 247));
+}
+a{
+    font-style: normal;
+    font-family: "lato",sans-serif;
+}
+p{
+    font-size: small;
+}
