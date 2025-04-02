6238e7098379e9a5d1019df1c38f04319dd26bc7
Rachana240906 <u24ai064@aid.svnit.ac.in>
2025-04-02 17:23:24 +0530

    Create ass 9(1).py

---
+++ ass 9(1).py
@@ -0,0 +1,37 @@
+HTML:
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biryani House Online Delivery</title>
+    <link rel="stylesheet" href="zomato_style.css">
+</head>
+<body >
+    <pre> 
+    <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoIycguxFgTpIN3L00tYQhJ2WkypXj5w_QkQ&s"><img  src="https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_300,q_100,fl_lossy,dpr_2.0,c_fit,f_auto,h_300/ohtwqyqn5xygec7epzug"width="940"height="400"></img>
+    </pre>
+    <pre>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShvWDE0TYK__AEyWn2L6VOmL1W0VuVd50-AA&s"alt="vegetarian"width="400"height="300"></img></img>   <img src="https://c.ndtvimg.com/2022-04/fq5cs53_biryani-doubletree-by-hilton_625x300_12_April_22.jpg"width="400"height="300">
+    <a href="https://www.zomato.com/surat/delivery?place_name=Surat&dishv2_id=52723&context=delivery"><h1>CLICK TO ORDER VEGETARIAN FOOD </h1></a>          <a href="https://www.zomato.com/surat/delivery?dishv2_id=55145&category=1"><h1>CLICK TO ORDER NON-VEGETARIAN FOOD</h1></a>
+    </pre>
+    <p><h3>NEW BRANCHES OPENING SOON!!</h3></p>
+    <a href="https://en.wikipedia.org/wiki/Zomato"><h2>ABOUT US</h2></a> 
+</body>
+</html>
+
+
+
+CSS:
+body{
+    background-image: url(https://static.vecteezy.com/system/resources/thumbnails/052/605/705/small/space-for-recipe-pizza-cooking-ingredients-on-black-background-top-view-photo.jpg);
+    background-size:contain;
+    background-repeat:repeat-y;
+}
+a{
+    color:antiquewhite;
+}
+h3{
+    color: brown;
+    border-color: black;
+}
